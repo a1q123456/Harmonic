c83ba805b2defd3dfee53da25264d5ff4bdaea4c
a1q123456 <a1q123456@gmail.com>
2017-07-13 00:35:12 +0800

    add demo project

---
+++ demo/Program.cs
@@ -0,0 +1,19 @@
+﻿using RtmpSharp.Net;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace demo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            RtmpServer server = new RtmpServer(new RtmpSharp.IO.SerializationContext());
+            server.RegisterApp("app");
+            server.Start();
+        }
+    }
+}
